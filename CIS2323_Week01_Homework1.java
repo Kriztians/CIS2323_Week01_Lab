5a91a3f750d2689117804456abb74a5eb20c1093
Kriztians <42624605+Kriztians@users.noreply.github.com>
2018-08-29 16:37:04 -0500

    Add files via upload

---
+++ CIS2323_Week01_Homework1.java
@@ -0,0 +1,18 @@
+public class CIS2323_Week01_Homework1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("============================================="); 
+		//name
+		System.out.println("Christian Reyes");
+		//Occupation and Phone
+		System.out.println("Programmer, OSU-OKC 	P:(405)240-8280");
+		//address and E-mail
+		System.out.println("1130 N Independence 	E:chreyes@okstate.edu");
+		//State and github account
+		System.out.println("Oklahoma city, OK	G:Github.com/Kriztians");
+		
+		System.out.println("=============================================");
+	}
+}
+	
